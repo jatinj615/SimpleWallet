ec35ec3725906fa9ab912ffd7c8a1386b2bc91d2
Jatin <jatinj615@gmail.com>
2018-03-27 20:42:09 +0530

    extend wallet

--- contracts/SimpleWallet.sol
+++ contracts/SimpleWallet.sol
@@ -2,7 +2,19 @@ pragma solidity^0.4.17;
 
 contract SimpleWallet {
     address owner;
-    mapping(address => bool) isAllowedToSendFundsMapping;
+    
+    struct WithdrawlStruct {
+        address to;
+        uint amount;
+    }
+
+    struct Senders {
+        bool allowed;
+        uint amount_sends;
+        mapping(uint => WithdrawlStruct) withdrawls;
+    }
+
+    mapping(address => Senders) isAllowedToSendFundsMapping;
 
     event Deposit(address _sender, uint amount);
     event Withdrawl(address _sender, uint amount, address _beneficiary);
@@ -12,16 +24,19 @@ contract SimpleWallet {
     }
 
     function() public payable{
-        require(msg.sender == owner || isAllowedToSendFundsMapping[msg.sender] == true);
+        require(msg.sender == owner || isAllowedToSendFundsMapping[msg.sender].allowed == true);
         Deposit(msg.sender, msg.value);
         
     }
 
     function sendFunds(uint amount, address receiver) public returns (uint) {
-        if(msg.sender == owner || isAllowedToSendFundsMapping[msg.sender]) {
+        if(isAllowedToSend(msg.sender)) {
             if(this.balance >= amount){
                 require(receiver.send(amount) == true);
                 Withdrawl(msg.sender, amount, receiver);
+                isAllowedToSendFundsMapping[msg.sender].amount_sends++;
+                isAllowedToSendFundsMapping[msg.sender].withdrawls[isAllowedToSendFundsMapping[msg.sender].amount_sends].to = receiver;
+                isAllowedToSendFundsMapping[msg.sender].withdrawls[isAllowedToSendFundsMapping[msg.sender].amount_sends].amount = amount;
                 return this.balance;
             }
         }
@@ -29,18 +44,18 @@ contract SimpleWallet {
 
     function allowAddressToSendMoney(address _address) public {
         if(msg.sender == owner) {
-            isAllowedToSendFundsMapping[_address] = true;
+            isAllowedToSendFundsMapping[_address].allowed = true;
         }
     }
 
     function disallowAddressToSendMoney(address _address) public {
         if(msg.sender == owner) {
-            isAllowedToSendFundsMapping[_address] = false;
+            isAllowedToSendFundsMapping[_address].allowed = false;
         }
     }
 
     function isAllowedToSend(address _address) public constant returns (bool) {
-        return isAllowedToSendFundsMapping[_address] || _address == owner;
+        return isAllowedToSendFundsMapping[_address].allowed || _address == owner;
     }
 
     function killWallet() public {
