23ade6ccd1907c7fb0e4d3ffdbcfcce4c04b7c2b
Jatin <jatinj615@gmail.com>
2018-03-31 13:57:14 +0530

    connected to web3

---
+++ src/js/app.js
@@ -0,0 +1,20 @@
+App = {
+    web3provider: null,
+    contract: {},
+
+    init: function(){
+        if (typeof web3 !== 'undefined') {
+            App.web3Provider = web3.currentProvider;
+          } else {
+            // If no injected web3 instance is detected, fall back to Ganache
+            App.web3Provider = new Web3.providers.HttpProvider('http://localhost:8545');
+          }
+          web3 = new Web3(App.web3Provider);          
+    }
+}
+
+$(function() {
+    $(window).load(function() {
+      App.init();
+    });
+});
