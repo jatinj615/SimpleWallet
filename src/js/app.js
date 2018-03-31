2f40a6867c19d494dfc8098d951c435194d12139
Jatin <jatinj615@gmail.com>
2018-03-31 21:44:32 +0530

    send funds view

--- src/js/app.js
+++ src/js/app.js
@@ -3,14 +3,29 @@ App = {
     contract: {},
 
     init: function(){
-        if (typeof web3 !== 'undefined') {
-            App.web3Provider = web3.currentProvider;
-          } else {
+        // if (typeof web3 !== 'undefined') {
+        //     App.web3Provider = web3.currentProvider;
+        //   } else {
             // If no injected web3 instance is detected, fall back to Ganache
-            App.web3Provider = new Web3.providers.HttpProvider('http://localhost:8545');
-          }
-          web3 = new Web3(App.web3Provider);          
+        App.web3Provider = new Web3.providers.HttpProvider('http://localhost:8545');
+        //   }
+        web3 = new Web3(App.web3Provider);
+        // web3.eth.getAccounts(function(err, acc){console.log(acc)});          
+    },
+
+    getAccounts: function(){
+        web3.eth.getAccounts(function(err, acc){
+            if(err){
+                console.log(err);
+            } else {
+                $('#accounts').append("<option></option>");
+                for(i = 0; i < acc.length; i++){
+                    $('#accounts').append("<option>"+acc[i]+"</option>");
+                }
+            }
+        });
     }
+
 
     
 }
@@ -19,4 +34,9 @@ $(function() {
     $(window).load(function() {
       App.init();
     });
+});
+
+$('#send-funds').click(function(){
+    $('#accounts').empty();
+    App.getAccounts();
 });
